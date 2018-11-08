ddc3e178bfa5ee89c55ad498e629ed92cff4af99
conor-gil <conor.gilly11@gmail.com>
2018-11-08 13:41:04 +0000

    added username and password input

--- gitapi.py
+++ gitapi.py
@@ -1,16 +1,16 @@
-#example from PyGithub
+import getpass
 from github import Github
 
 # First create a Github instance:
 
+user = input("Github Username: ")
+pw = getpass.getpass("Password: ")
+
 # using username and password
-#g = Github("user", "password")
+g = Github(user, pw)
 
 # or using an access token
-g = Github("fa9aff87f25bf654509cabc6748346b685906eed")
-
-# Github Enterprise with custom hostname
-#g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
+#g = Github("fa9aff87f25bf654509cabc6748346b685906eed")
 
 # Then play with your Github objects:
 for repo in g.get_user().get_repos():
