70c6e2939c18532ebc13198e6407b38e201e6427
conor-gil <conor.gilly11@gmail.com>
2018-11-26 22:49:57 +0000

    added django project with example d3 html

--- gitapi.py
+++ gitapi.py
@@ -15,3 +15,5 @@ g = Github(user, pw)
 # Then play with your Github objects:
 for repo in g.get_user().get_repos():
     print(repo.name)
+    for commit in repo.get_commits():
+        print(commit.commit.author.date)
