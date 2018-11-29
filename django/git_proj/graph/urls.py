6e480b39c0b813717c4b12ff9537deb78af2fa98
conor-gil <conor.gilly11@gmail.com>
2018-11-29 16:25:59 +0000

    added different views

--- django/git_proj/graph/urls.py
+++ django/git_proj/graph/urls.py
@@ -1,8 +1,10 @@
 # graph/urls.py
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, PiePageView, HistoPageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('pie/', PiePageView.as_view(), name='pie'),
+    path('histo/', HistoPageView.as_view(), name='histo')
 ]
--- django/git_proj/graph/views.py
+++ django/git_proj/graph/views.py
@@ -1,5 +1,12 @@
 # pages/graph.py
 from django.views.generic import TemplateView
+from django.shortcuts import render
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class PiePageView(TemplateView):
+    template_name = 'pie.html'
+
+class HistoPageView(TemplateView):
+    template_name = 'histo.html'
