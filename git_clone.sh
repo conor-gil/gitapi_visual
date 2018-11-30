80d17e0df1cffd08d67751e39c7b8216cf682168
conor-gil <conor.gilly11@gmail.com>
2018-11-30 16:09:00 +0000

    added and updated shell scripts

---
+++ git_clone.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/conor-gil/gitapi_visual;
--- update_git.sh
+++ update_git.sh
@@ -1,2 +1,4 @@
+git config user.email "conor.gilly11@gmail.com"
+git config user.name "conor-gil"
 git commit -a -m 'run test';
 git push;
